56f91773b2c8e375ba4d38f7a2a7566c2df7b92e
amurrai <andre.murrai@gmail.com>
2024-01-22 09:44:43 -0300

    Add Queries 3 - Groupby assignments

---
+++ 3_queries_group-by/3_cohorts_over_18_students.sql
@@ -0,0 +1,5 @@
+SELECT cohorts.name as cohort_name, count(*) as student_count 
+FROM cohorts JOIN students ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING count(*) >= 18
+ORDER BY count(*);
