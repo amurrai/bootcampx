8bb6d020a9667be0dfdf11f553859a22c5b9ed81
amurrai <andre.murrai@gmail.com>
2024-01-25 11:23:48 -0300

    Add Node Postgres node assignments

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+const args = process.argv.slice(2);
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.*) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name like '%${args[0]}%'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
