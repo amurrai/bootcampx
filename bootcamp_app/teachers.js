e63d26d2718f1e19259f698c7f88ddb36ad4cbdd
amurrai <andre.murrai@gmail.com>
2024-01-26 01:11:16 -0400

    Add SQL Injection assignments

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = args[0];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.*) as total_assistances
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name like '%${args[0]}%'
+WHERE cohorts.name like $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
-`)
+`
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
